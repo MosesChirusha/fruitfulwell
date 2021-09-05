1f2efe72440c3f984a72e7334f428e2724e80046
Moses Chirusha <moseschirusha@gmail.com>
2021-09-05 15:49:59 +0200

    Renaiming of menu

--- admin/templates/menu.php
+++ admin/templates/menu.php
@@ -2,7 +2,7 @@
 <aside class="menu-sidebar d-none d-lg-block">
     <div class="logo">
         <a href="index.php">
-            <img src="images/icon/logo.png" alt="Cool Admin" />
+            <img src="images/icon/fruitful-well.png" alt="Fruitful Well" />
         </a>
     </div>
     <div class="menu-sidebar__content js-scrollbar1">
@@ -10,95 +10,54 @@
             <ul class="list-unstyled navbar__list">
                 <li class="active has-sub">
                     <a class="js-arrow" href="#">
-                        <i class="fas fa-tachometer-alt"></i>Dashboard</a>
+                        <i class="fas fa-tachometer-alt"></i>Articles</a>
                     <ul class="list-unstyled navbar__sub-list js-sub-list">
                         <li>
-                            <a href="index.php?title=Dashboard">E-commerce</a>
+                            <a href="blog.php">Compose Article</a>
                         </li>
                         <li>
-                            <a href="blog.php?title=Dashboard">Blog</a>
-                        </li>
-                        <li>
-                            <a href="index3.html">Dashboard 3</a>
-                        </li>
-                        <li>
-                            <a href="index4.html">Dashboard 4</a>
+                            <a href="#">Recent Articles</a>
                         </li>
                     </ul>
                 </li>
                 <li>
-                    <a href="chart.html">
+                    <a href="#">
                         <i class="fas fa-chart-bar"></i>Charts</a>
                 </li>
                 <li>
-                    <a href="table.html">
+                    <a href="#">
                         <i class="fas fa-table"></i>Tables</a>
                 </li>
                 <li>
-                    <a href="user.php?title=Users">
+                    <a href="user.php">
                         <i class="far fa-check-square"></i>Users</a>
                 </li>
                 <li>
-                    <a href="calendar.html">
+                    <a href="#">
                         <i class="fas fa-calendar-alt"></i>Calendar</a>
                 </li>
                 <li>
-                    <a href="map.html">
+                    <a href="#">
                         <i class="fas fa-map-marker-alt"></i>Maps</a>
                 </li>
                 <li class="has-sub">
                     <a class="js-arrow" href="#">
-                        <i class="fas fa-copy"></i>Pages</a>
+                        <i class="fas fa-copy"></i>Profile</a>
                     <ul class="list-unstyled navbar__sub-list js-sub-list">
                         <li>
-                            <a href="login.php">Login</a>
+                            <a href="#">Account</a>
                         </li>
                         <li>
-                            <a href="register.html">Register</a>
+                            <a href="#">Settings</a>
                         </li>
                         <li>
-                            <a href="forget-pass.html">Forget Password</a>
+                            <a href="#">Licence and Accreditation</a>
                         </li>
                     </ul>
                 </li>
                 <li class="has-sub">
                     <a class="js-arrow" href="#">
-                        <i class="fas fa-desktop"></i>UI Elements</a>
-                    <ul class="list-unstyled navbar__sub-list js-sub-list">
-                        <li>
-                            <a href="button.html">Button</a>
-                        </li>
-                        <li>
-                            <a href="badge.html">Badges</a>
-                        </li>
-                        <li>
-                            <a href="tab.html">Tabs</a>
-                        </li>
-                        <li>
-                            <a href="card.html">Cards</a>
-                        </li>
-                        <li>
-                            <a href="alert.html">Alerts</a>
-                        </li>
-                        <li>
-                            <a href="progress-bar.html">Progress Bars</a>
-                        </li>
-                        <li>
-                            <a href="modal.html">Modals</a>
-                        </li>
-                        <li>
-                            <a href="switch.html">Switchs</a>
-                        </li>
-                        <li>
-                            <a href="grid.html">Grids</a>
-                        </li>
-                        <li>
-                            <a href="fontawesome.html">Fontawesome Icon</a>
-                        </li>
-                        <li>
-                            <a href="typo.html">Typography</a>
-                        </li>
-                    </ul>
+                        <i class="fas fa-desktop"></i>Logout</a>
                 </li>
             </ul>
         </nav>
